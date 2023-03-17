533c84cc66be17191a9f3a19129ffa1e555a5a53
student <student@K29-USER9>
2023-03-17 11:24:52 +0200

    Add project files.

---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,30 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.GreenYellow;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.SlateGray;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.SlateGray;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
